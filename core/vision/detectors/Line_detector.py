3d6ef34597376cca3cf074ddfc08be3e7789bb0f
francois <francois.gagne.7@ens.etsmtl.ca>
2026-02-01 18:05:59 -0500

    test

--- core/vision/detectors/Line_detector.py
+++ core/vision/detectors/Line_detector.py
@@ -49,8 +49,6 @@ class LineFollowerControl:
         self.base_speed = base_speed
 
     def compute_commands(self, line_error):
-        kp = 0.1
-        base_speed = 1
         #if line_error > 20 or line_error < -20:
         #    return 0, 0  # On s'arrête si on perd la ligne
         print(line_error)
@@ -62,5 +60,5 @@ class LineFollowerControl:
         left_speed = self.base_speed + turn_output
         right_speed = self.base_speed - turn_output
         print("left speed = ", left_speed, "right speed= ", right_speed, "turn outpu = ", turn_output)
-        #return int(left_speed), int(right_speed)
-        return 0, 0 
+        return int(left_speed), int(right_speed)
+      
