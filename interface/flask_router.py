4355f5900b4baa2b003fb9fdc94dc62243779c31
Olivier poitras <olivier.poitras.2@ens.etsmtl.ca>
2026-02-02 22:45:33 -0500

    Ajout des routes manquantes pour le pont

--- interface/flask_router.py
+++ interface/flask_router.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# flask_server.py
-# ------------------
-"""Définit les routes Flask et les lie aux méthodes du contrôleur backend.
-    On déclare ici une route pour chaque bouton ou action du serveur vers
-    une méthode du contrôleur.
-"""
+# flask_router.py
 
 def register_routes(ctrl):
     app = ctrl.app
 
-    # Pages
+    # Pages Web
     app.add_url_rule('/', 'home', lambda: ctrl.home())
     app.add_url_rule('/vision', 'vision', lambda: ctrl.vision())
     app.add_url_rule('/onglet_template', 'onglet_template', lambda: ctrl.onglet_template())
 
-    # MISC actions
+    # Système
     app.add_url_rule('/exit', 'exit_server', lambda: ctrl.exit_server(), methods=['POST'])
 
-    # Vision actions
+    # Caméra & Vision
     app.add_url_rule('/download_image/<filename>', 'download_image', lambda filename: ctrl.download_image(filename))
     app.add_url_rule('/capture_image', 'capture_image', lambda: ctrl.capture_image(), methods=['POST'])
     app.add_url_rule('/status', 'status', lambda: ctrl.status())
@@ -26,11 +21,17 @@ def register_routes(ctrl):
     app.add_url_rule('/close_camera', 'close_camera', lambda: ctrl.close_camera(), methods=['POST'])
     app.add_url_rule('/start_camera', 'start_camera', lambda: ctrl.start_camera(), methods=['POST'])
 
-    # Moteur
+    # Zumi Moteurs
     app.add_url_rule('/zumi/forward', 'forward', lambda: ctrl.forward())
     app.add_url_rule('/zumi/reverse', 'reverse', lambda: ctrl.reverse())
     app.add_url_rule('/zumi/left', 'left', lambda: ctrl.left())
     app.add_url_rule('/zumi/right', 'right', lambda: ctrl.right())
     app.add_url_rule('/zumi/stop', 'stop', lambda: ctrl.stop())
+
+    # --- PONT (Nouveaux liens) ---
+    app.add_url_rule('/bridge/open', 'bridge_open', lambda: ctrl.bridge_open(), methods=['POST'])
+    app.add_url_rule('/bridge/close', 'bridge_close', lambda: ctrl.bridge_close(), methods=['POST'])
+    app.add_url_rule('/bridge/green', 'bridge_green', lambda: ctrl.bridge_green(), methods=['POST'])
+    app.add_url_rule('/bridge/red', 'bridge_red', lambda: ctrl.bridge_red(), methods=['POST'])
 
     return app
