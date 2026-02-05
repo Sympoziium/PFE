9c7f10c993d3c0ec278e75350162a976b07d4ee0
francois <francois.gagne.7@ens.etsmtl.ca>
2026-02-05 17:20:28 -0500

    -out

--- main.py
+++ main.py
@@ -76,10 +76,10 @@ def control_loop():
             #l_speed, r_speed = follower.compute_commands(line_val)
             #print('L = ', l_speed, 'R = ', r_speed)
             output = follower.compute_commands(line_val)
-            print('output = ', output)
+            print('output = ', -output)
             if (3 < abs(line_val) <80):
                            
-                zumi.turn(output, duration=1.5, max_speed=25, accuracy=1)
+                zumi.turn(-output, duration=1.5, max_speed=25, accuracy=1)
                 #zumi.control_motors(r_speed, l_speed)
         else:
             zumi.stop()
