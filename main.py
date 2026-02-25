50e8f359682b1ea1b80ecec0cc6e2f28ac23112e
Sympoziium <cedricsenecal@hotmail.com>
2026-02-25 18:27:27 -0500

    fix: Adjust Haar classifier parameters for pedestrian detection

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ MODELS_DIR = os.path.join(os.path.dirname(__file__), 'core', 'vision', 'detector
 # config du classificateur de Haar pour l'ajout de détecteurs
 haar_classifier.add_classifier('stop_sign', os.path.join(MODELS_DIR, 'stop_sign_classifier_2.xml'), scaleFactor=1.1, minNeighbors=5)
 # haar_classifier.add_classifier('Pieton', os.path.join(MODELS_DIR, 'pedestrian_classifier.xml'))
-haar_classifier.add_classifier('Pieton', os.path.join(MODELS_DIR, 'LBP_Alpha_Prime.xml'), scaleFactor=1.1, minNeighbors=2)
+haar_classifier.add_classifier('Pieton', os.path.join(MODELS_DIR, 'LBP_Alpha_Prime.xml'), scaleFactor=1.08, minNeighbors=2)
 
 # On ajoute le détecteur au pipeline de vision
 # vision_pipeline.add_detectors(Lum_detector)
